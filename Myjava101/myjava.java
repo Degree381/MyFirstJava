a253e6f32fb54108b537a04d4d2eb4cfb3c9aedd
Degree381 <111416247+Degree381@users.noreply.github.com>
2022-08-17 09:19:50 +0200

    ADDED NEW MESSAGE

--- Myjava101/myjava.java
+++ Myjava101/myjava.java
@@ -4,6 +4,8 @@ public  class myjava{
 
  public static void main(String[] args) {
       System.out.println("Hello World!");
+       // making changes here by Themba
 
+       System.out.println("Tomorrow is CT1 TEST AND ITS THEORY!");
  }   
 }
